56d01d41dae83031d3e280c88531ced539461ce8
Rio <riolaney@att.net>
2024-02-05 22:18:49 -0600

    Logic for books

--- src/simple_server.php
+++ src/simple_server.php
@@ -1,7 +1,98 @@
-<?php 
-  $mydata = $_REQUEST["mydata"];
+<?php
 
-  sleep(5);
 
-  print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
+function addStudentToCourse($name, $title) {
+  // find the course in file
+  $student = array(
+    // Name
+    "books" => array (
+
+    )
+  );
+
+  $student["name"] = $name;
+  $student["books"][0]["title"] = $title;
+  
+  return $student;
+}
+
+function createBook($title, $bpub, $bed, $dop) {
+  $book = array();
+  $book["title"] = $title;
+  $book["bpub"] = $bpub;
+  $book["bed"] = $bed;
+  $book["dop"] = $dop;
+
+  
+  return $book;
+}
+
+function createCourse($name, ...$books) {
+  $course = array();
+  $course["course"] = $name;
+  $course["students"] = array();
+  $course["books"] = array();
+
+  foreach ($books as $book) {
+    $course["books"] += $book;
+  }
+  
+
+  return $course;
+}
+
+
+
+
+function writeJson($data) {
+ // $fp = fopen("data.json", 'w+');
+
+  $inp = file_get_contents("array.json");
+  $tempArray = json_decode($inp);
+  //$idk = array($tempArray);
+
+  array_push($tempArray, $data);
+  $jsonData = json_encode($tempArray);
+  file_put_contents("array.json", $jsonData);
+//  fclose($fp);
+
+
+}
+
+print "<p>Hi, ! This is new important data for your web page.</p> ";
+
+// get the q parameter from URL
+if (($_GET['instructor'] ?? true)) {
+  $isInstructor = $_GET['instructor'];
+  echo $isInstructor;
+  // handle instructor
+  if ($isInstructor === "true") {
+    // an instructor can do what?
+    $course = $_GET['course'];
+    $title = $_GET['title'];
+    $bpub = $_GET['pub'];
+    $bed = $_GET['ed'];
+    $dop = $_GET['dop'];
+
+    $book = createBook($title, $bpub, $bed, $dop);
+
+    // search course
+    // if found course write to it
+
+    $write = createCourse($course, $book);
+    // if no course found make new one
+    writeJson($write);
+
+  } else {
+    // a student can do what
+    echo "124rdajkfjka";
+    echo "pls";
+  }
+
+
+} else {
+
+
+
+}
 ?>
